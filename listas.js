78de3cea19cccc4be0b4ba08a9abcffeea66eaa2
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-26 18:33:28 -0500

    Fifth Execise, Lists

---
+++ listas.js
@@ -0,0 +1,56 @@
+// ------------------------ PARTE # 1 ---------------------------------- //
+// 1. ¿Qué es un array? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 2. ¿Qué es un objeto? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 3. ¿Cuándo es mejor usar objetos o arrays? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 4. ¿Puedo mezclar arrays con objetos o incluso objetos con arrays? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+
+// ------------------------ PARTE # 2 ---------------------------------- //
+// 1. Crea una función que pueda recibir cualquier array como parámetro e imprima su primer elemento.
+
+
+
+
+
+// 2. Crea una función que pueda recibir cualquier array como parámetro e imprima todos sus elementos uno por uno (no se vale imprimir el array completo).
+
+
+
+
+
+// Crea una función que pueda recibir cualquier objeto como parámetro e imprima todos sus elementos uno por uno (no se vale imprimir el objeto completo).
