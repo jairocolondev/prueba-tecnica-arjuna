c1e8fc572d53c93acb564f44a55081fe5118b01e
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-26 15:05:01 -0500

    First Exercise

---
+++ variables.js
@@ -0,0 +1,85 @@
+// ------------------------ PARTE # 1 ---------------------------------- //
+
+
+// 1. ¿Que es una variable y para que sirve? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 2. ¿Cuál es la diferencia entre declarar e inicializar una variable? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 3. ¿Cuál es la diferencia entre sumar números y concatenar strings? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 4. ¿Cuál es la diferencia entre sumar números y concatenar strings? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+// 5. ¿Qué es la memoria RAM y para que sirve? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 6. ¿Cuál operador me permite sumar o concatenar? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 7. ¿Cuál operador me permite sumar o concatenar? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// ------------------------------ PARTE # 2 ----------------------------------- //
+
+// 1. Escribe el tipo de dato al que representa cada elemento de esta lista //
+
+//Nombre ---------------------------------------->
+//Nombre de usuario ----------------------------->
+//Edad ------------------------------------------>
+//Correo electrónico ---------------------------->
+//Número de celular ----------------------------->
+//Tiene mascotas -------------------------------->
+//Mascotas (perro, gato, gallina, girafa) ------->
+//Tiene carro ----------------------------------->
+//Dinero Ahorrado -------------------------------> 
+//Mayor de edad --------------------------------->
+//Dos prestamos --------------------------------->
+
+
+// 2. Traduce a código JavaScript el ejemplo anterior //
+
+
+
+// 3. Calcula e imprime las siguientes variables a partir de las variables del ejemplo anterior: //
+//Nombre completo (nombre y apellido)
+//Dinero real (dinero ahorrado menos deudas)
+//Cuantas mascotas tiene y mostrar los nombres
+
+
