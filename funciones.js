3b1f1f7c56a193bfb60f7945d2413887c53b2826
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-26 16:09:33 -0500

    Second Exercise, Functions

---
+++ funciones.js
@@ -0,0 +1,53 @@
+// ------------------------ PARTE # 1 ---------------------------------- //
+// RESPONDE LAS SIGUIENTES PREGUNTAS //
+
+// 1. ¿Qué es una función? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 2. ¿Cuales son los tipos de funciones que tenemos? -- Escribe la sintaxis //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 2. ¿Cuales son las clases de funciones que tenemos? -- Escribe la sintaxis //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// 3. ¿Cuándo me sirve usar una función en mi código? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+
+// 4. ¿Cuál es la diferencia entre parámetros y argumentos de una función? //
+
+/*TU RESPUESTA:
+
+
+*/
+
+
+// ------------------------ PARTE # 2 ---------------------------------- //
+//Convierte el siguiente código en una función, pero, cambiando cuando sea necesario las variables constantes por parámetros y argumentos en una función: //
+
+let name = "Jairo";
+let lastname = "Colón";
+let nickname = "Jey Colón";
+let completeName = name + lastname;
+
+console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
