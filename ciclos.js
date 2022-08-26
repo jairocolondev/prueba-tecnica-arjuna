152a9dc795fe83588d3b4816aa4709102cd9f87b
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-26 18:03:01 -0500

    Fourth Exercise, cycles

---
+++ ciclos.js
@@ -0,0 +1,62 @@
+// ------------------------ PARTE # 1 ---------------------------------- //
+// Responde las siguientes preguntas a continuación:
+
+// 1. ¿Qué es un ciclo? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 2. ¿Qué tipos de ciclos conoces en JavaScript? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 3. ¿Qué es un ciclo infinito y por qué es un problema? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 4. ¿Puedo mezclar ciclos y condicionales? //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// 5. Escribe la sintaxis de un ciclo for //
+
+/*TU RESPUESTA:
+
+
+
+*/
+
+
+
+// ------------------------ PARTE # 2 ---------------------------------- //
+
+// Escribe un código en JavaScript que le pregunte a los usuarios cuánto es 10 + 10. Si responden bien, mostramos un mensaje de felicitaciones, pero si responden mal, muestras un mensaje de incorrecto vuelve a intentarlo.//
+
+/* TU RESPUESTA:
+
+
+
+*/
