11f3fbd12fb11cc57e73fb75134dfe83b477d042
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-26 20:03:53 -0500

    Sixth Execise, logic test

---
+++ ejercicios.js
@@ -0,0 +1,52 @@
+//---PARA ESTOS EJERCICIOS TIENES QUE HACER UNO POR UNO PARA QUE PUEDAS PROBAR EL RESULTADO---//
+
+
+
+/* EJERCICIO N° 1*/
+/* Crea un objeto user y agregar las siguientes propiedades:
+(
+nombre, 
+apellido, 
+edad, 
+ubicación {
+  país
+  ciudad
+  dirección
+},
+amigos: ["amigo 1", "amigo 2", "amigo 3"],
+active:
+sendMail() {
+  devolver un mensaje de enviando email...
+}
+
+Acceder a cada propiedad y mostrar su valor
+
+) */
+
+
+
+
+
+
+
+/* EJERCICIO N° 2*/
+//Agregar, un elemento al principio del array, Agregar, un elemento al final del array, y mostrar por consola
+//Eliminar el último elemento y luego eliminar el primer elemento
+//Recorrer el array e imprimir el elemento en la tercera posición
+//Eliminar un elemento a partir de la posición 2 del array,
+
+const lenguajes = ["JavaScript", "Python", "Go", "Java", "C++", "C+", "Kotlin"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
