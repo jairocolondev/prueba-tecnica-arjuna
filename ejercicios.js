4a0e4e000e8c75d9728d22290941feea968d7146
Arjuna Diazgranados <arjuna.desarrollador@gmail.com>
2022-08-26 21:00:52 -0500

    Respuestas ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -23,10 +23,25 @@ Acceder a cada propiedad y mostrar su valor
 
 ) */
 
+// let user = {
+//   nombre: "Laura ",
+//   apellido: "Perez ",
+//   edad: 30 ,
+//   ubicacion: {
+//     pais: "Colombia ",
+//     ciudad: "Bucaramanga ",
+//     direccion: "calle 23 # 12 54 ",
+//   },
+//   amigos: ["Roberto, ", "Juanita, ", "Juan. "],
+//   active: true,
+//   sendmail(){
+//     return "enviando mensaje..."
+//   }
+// }
 
 
-
-
+// let userInformation = user.nombre + user.apellido + user.ubicacion.pais + user.ubicacion.ciudad + user.ubicacion.direccion + user.amigos[0] +user.amigos[1] + user.amigos[2] + user.active + user.sendmail;
+// console.log(userInformation)
 
 
 /* EJERCICIO N° 2*/
@@ -35,9 +50,16 @@ Acceder a cada propiedad y mostrar su valor
 //Recorrer el array e imprimir el elemento en la tercera posición
 //Eliminar un elemento a partir de la posición 2 del array,
 
-const lenguajes = ["JavaScript", "Python", "Go", "Java", "C++", "C+", "Kotlin"]
-
-
+ const lenguajes = ["JavaScript", "Python", "Go", "Java", "C++", "C+", "Kotlin"]
+       let agregarUnElemento = lenguajes.unshift("HTML");
+       let eliminarUltimoElemento = lenguajes.pop();
+       let eliminarPrimerElementlo = lenguajes.shift();
+       let Recorrerarray = lenguajes[2];
+       let pos = 1;
+       let EliminarUnElemento = lenguajes.splice(2,pos)
+      console.log(lenguajes);
+      console.log(Recorrerarray);
+      console.log(EliminarUnElemento);
 
 
 
